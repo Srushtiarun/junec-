a42e4cff617b511c83fc29594661f5ebee2c2408
Srushtiarun <66377359+Srushtiarun@users.noreply.github.com>
2020-06-27 12:38:18 +0530

    project3

---
+++ votecheck.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int age;
+  cout<<"enter your age"<<endl;
+  cin>>age;
+  if(age<18)
+        cout<<"not eligible to vote"<<endl;
+  else
+    cout<<"eligible to vote"<<endl;
+
+}
