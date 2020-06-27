824f53bf19880f1e667536081d49f284d991f776
Srushtiarun <66377359+Srushtiarun@users.noreply.github.com>
2020-06-27 12:31:01 +0530

    1st and 2nd project

--- size.cpp
+++ size.cpp
@@ -6,4 +6,12 @@ cout<<"Size of char"<<sizeof(char)<<endl;
 cout<<"Size of int"<<sizeof(int)<<endl;
 cout<<"Size of float"<<sizeof(float)<<endl;
 cout<<"Size of double"<<sizeof(double)<<endl;
+ //to find product of 2 numbers 
+    int x,y,z;
+  cout<<"enter two numbers"<<endl;
+  cin>>x>>y;
+  z=x*y;
+  cout<<"product of two entered numbers is"<<z<<endl;
+
 }
+
