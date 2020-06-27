8669d3d0c54dad8171b91626ccc62aabc8e9dcfb
Srushtiarun <66377359+Srushtiarun@users.noreply.github.com>
2020-06-27 12:25:08 +0530

    product of 2 nubers

---
+++ assignment2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x,y,z;
+  cout<<"enter two numbers"<<endl;
+  cin>>x>>y;
+  z=x*y;
+  cout<<"product of two entered numbers is"<<z<<endl;
+
+}
